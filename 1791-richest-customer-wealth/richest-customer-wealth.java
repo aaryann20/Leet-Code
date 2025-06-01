1881d5fc05ab82fb5806354b92e8ece92191c06e
Aaryan Soni <121357513+aaryann20@users.noreply.github.com>
2025-06-01 19:31:51 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (14.16%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.java
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -7,9 +7,10 @@ class Solution {
             int max = 0;
             for(int j = 0;j<accounts[i].length;j++){
                 max = accounts[i][j] + max;
-                Mmax = Math.max(max,Mmax);
+               
 
             }
+             Mmax = Math.max(max,Mmax);
         }
         return Mmax;
         
