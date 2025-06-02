29be3d22dbe0af34fb9da1aa9879247c8d7590ad
Aaryan Soni <121357513+aaryann20@users.noreply.github.com>
2025-06-03 00:09:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (22.66%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,33 +1,29 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         int n = nums.length;
-        int st = 0;
-        int end = n - 1;
+        
+        if(n==1) return 0;
 
-        // Handle edge cases for single element arrays
-        if (n == 1) {
+        if(nums[0]>nums[1]){
             return 0;
         }
 
-        while (st <= end) {
-            int mid = st + (end - st) / 2;
+        if(nums[n-1]>nums[n-2]){
+            return n-1;
+        }
+        int st = 1;
+        int end = n-2;
 
-            // Check if mid is a peak element
-            boolean isLeftSmaller = (mid == 0 || nums[mid] > nums[mid - 1]);
-            boolean isRightSmaller = (mid == n - 1 || nums[mid] > nums[mid + 1]);
-
-            if (isLeftSmaller && isRightSmaller) {
+        while(st<=end){
+            int mid = st+(end-st)/2;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
                 return mid;
-            }
-
-            // If the right neighbor is greater, search the right half
-            if (mid < n - 1 && nums[mid] < nums[mid + 1]) {
-                st = mid + 1;
-            } else { // Otherwise, search the left half
-                end = mid - 1;
+            }else if(nums[mid]>nums[mid-1]){
+                st = mid +1;
+            }else{
+                end = mid -1;
             }
         }
-
-        return -1; // This line won't be reached for valid input
+        return -1;
     }
 }
